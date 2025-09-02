7661228d313efb0e02e86f573bedd3b8294b83f6
DiegoRg26 <darg17@outlook.com>
2025-09-02 13:57:16 -0500

    =========[CAMBIOS]=========
    * Solucion de error de CORS en el archivo 'App.php'.

--- src/App/App.php
+++ src/App/App.php
@@ -10,10 +10,10 @@ $app = AppFactory::create();
 $container = $app->getContainer();
 $app->add(function($request, $handler){
     $response = $handler->handle($request);
-    return $response;
-    // ->withHeader('Access-Control-Allow-Origin','*')
-    //         ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-    //         ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    return $response
+    ->withHeader('Access-Control-Allow-Origin','*')
+    ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
 });
 
 
