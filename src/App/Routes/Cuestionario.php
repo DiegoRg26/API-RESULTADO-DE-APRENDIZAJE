861efe1f0eda4b204ef7473b858ba9aadc4c245b
DiegoRg26 <darg17@outlook.com>
2025-09-10 14:08:12 -0500

    =========[CAMBIOS]=========
    * Bug fixes

--- src/App/Routes/Cuestionario.php
+++ src/App/Routes/Cuestionario.php
@@ -9,18 +9,18 @@ $group->group('/cuestionario', function(RouteCollectorProxy $subgroup){
     // Obtener cuestionarios abiertos con estado
     $subgroup->get('/abiertos', 'App\Controllers\MenuCuestionario_controller:getCuestionariosAbiertos');
 
-    // Obtener estado de un cuestionario (progreso guardado)
-    $subgroup->get('/resolver/estado', 'App\Controllers\resolver_controller:getEstado');
-    
     //Obtener informacion de un cuestionario
     $subgroup->get('/{cuestionario_id}', 'App\Controllers\MenuCuestionario_controller:getCuestInfo');
-
+    
     // Verificar cuestionario
     $subgroup->get('/{cuestionario_id}/verificar', 'App\Controllers\resolver_controller:verificarResolucion');
-
+    
     // Obtener programas disponibles para crear cuestionario
     // $subgroup->post('/programas-disponibles', 'App\Controllers\crearCuestionario_controller:getProgramasDisponibles');
     
+    // Obtener estado de un cuestionario (progreso guardado)
+    $subgroup->post('/resolver/estado', 'App\Controllers\resolver_controller:getEstado');
+
     // Crear cuestionario
     $subgroup->post('/crear', 'App\Controllers\crearCuestionario_controller:crearCuestionario');
     
